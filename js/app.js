ea0ef9cb615669d7a7466c086f26c8e712fcae02
Wenhao Piao <wenhaopiao520@gmail.com>
2021-01-16 21:13:22 -0800

    Complete Lab-03

--- js/app.js
+++ js/app.js
@@ -4,81 +4,88 @@
 let userName = prompt('Hello there, may I ask what is your name?');
 alert(`Welcome to my page, ${userName}. Please participate in a fun guessing game.`);
 
-let correctStatement = 'You are correct!';
-let wrongStatement = 'You are wrong!';
-let invalidStatement = 'You typed a invalid answer!';
-// 5 questions; only accepts yes/no or y/n; case insensitive
-// quesiont #1
-let questionOne = 'Am I from Seattle? Type yes/no or y/n.';
-let answerOne = prompt(questionOne).toLowerCase();
-if (answerOne === 'yes' || answerOne === 'y') {
-  // console.log(correctStatement);
-  alert(correctStatement);
-} else if (answerOne === 'no' || answerOne === 'n') {
-  // console.log(wrongStatement);
-  alert(wrongStatement);
-} else {
-  // console.log(invalidStatement);
-  alert(invalidStatement);
+// use For Loop to ask first five questions and use corresponding respons based on users answers.
+// question #1 - #5
+let answerStatements = ['You are correct!', 'You are wrong!', 'You typed a invalid answer!'];
+let questions = [
+  'Question 1: Am I from Seattle? Type yes/no or y/n.',
+  'Question 2: Do I like dogs? Type yes/no or y/n.',
+  'Question 3: Is my favorite sport is basketball? Type yes/no or y/n.',
+  'Question 4: Is my favorite color is yellow? Type yes/no or y/n.',
+  'Question 5: Am I female? Type yes/no or y/n.'
+];
+let myAnswers = [
+  ['yes', 'y', 'no', 'n'],
+  ['yes', 'y', 'no', 'n'],
+  ['no', 'n', 'yes', 'y'],
+  ['yes', 'y', 'no', 'n'],
+  ['no', 'n', 'yes', 'y']
+];
+let numberOfCorrectAnswer = 0;
+
+for (let i = 0; i < questions.length; i++) {
+  // console.log(questions[i]);
+  let userInput = prompt(questions[i]).toLowerCase();
+  if (userInput === myAnswers[i][0] || userInput === myAnswers[i][1]) {
+    // console.log(answerStatements[0]);
+    numberOfCorrectAnswer++;
+    alert(answerStatements[0]);
+  } else if (userInput === myAnswers[i][2] || userInput === myAnswers[i][3]) {
+    // console.log(answerStatements[1]);
+    alert(answerStatements[1]);
+  } else {
+    // console.log(answerStatements[2]);
+    alert(answerStatements[2]);
+  }
 }
 
-// quesiont #2
-let questionTwo = 'Do I like dogs? Type yes/no or y/n.';
-let answerTwo = prompt(questionTwo).toLowerCase();
-if (answerTwo === 'yes' || answerTwo === 'y') {
-  // console.log(correctStatement);
-  alert(correctStatement);
-} else if (answerTwo === 'no' || answerTwo === 'n') {
-  // console.log(wrongStatement);
-  alert(wrongStatement);
-} else {
-  // console.log(invalidStatement);
-  alert(invalidStatement);
+// question #6
+let numberInMind = Math.floor(Math.random() * 16) + 1;
+console.log(numberInMind);
+for (let chances = 4; chances > 0; chances--) {
+  let answerSix = prompt(`Question 6: Guess a number (1-16) that in my mind. You have ${chances} chances left!`);
+  if (parseInt(answerSix) === numberInMind) {
+    numberOfCorrectAnswer++;
+    alert(`You are correct. The number in my mind was ${numberInMind}!`);
+    break;
+  } else if (parseInt(answerSix) < numberInMind) {
+    alert('It is too low, try a larger number!');
+  } else if (parseInt(answerSix) > numberInMind) {
+    alert('It is too high, try a smaller number!');
+  } else {
+    alert('Your input is invalid. Please Try an integer number from 1 to 16.');
+  }
+
+  if (chances === 1) {
+    alert(`You already used all 4 chances. The number in my mind was ${numberInMind}!`);
+  }
 }
 
-// question #3
-let questionThree = 'Is my favorite sport is basketball? Type yes/no or y/n.';
-let answerThree = prompt(questionThree).toLowerCase();
-if (answerThree === 'no' || answerThree === 'n') {
-  // console.log(correctStatement);
-  alert(correctStatement);
-} else if (answerThree === 'yes' || answerThree === 'y') {
-  // console.log(wrongStatement);
-  alert(wrongStatement);
-} else {
-  // console.log(invalidStatement);
-  alert(invalidStatement);
+// question #7
+let correctAnswers = ['yellow', 'blue', 'green'];
+let attemps = 6;
+let findMatch = false;
+// Loop will end once the user guesses a correct answer or they run out of attemps.
+while (attemps && !findMatch) {
+  let answerSeven = prompt(`Question 7: Please guess one of my three favorite colors. You have ${attemps} attemps left!`).toLowerCase();
+  for (let i = 0; i < correctAnswers.length; i++) {
+    if (answerSeven === correctAnswers[i]) {
+      findMatch = true;
+      break;
+    }
+  }
+
+  attemps--;
+  if (findMatch) {
+    numberOfCorrectAnswer++;
+    alert(`You are correct! My three favorite colors are ${correctAnswers[0]}, ${correctAnswers[1]}, and ${correctAnswers[2]}.`);
+    break;
+  } else if (attemps > 0) {
+    alert(`You are wrong. Please try again, you have ${attemps} attemps left!`);
+  } else {
+    alert(`You already used all your 6 attemps. My three favorite colors are ${correctAnswers[0]}, ${correctAnswers[1]}, and ${correctAnswers[2]}.`);
+  }
 }
 
-// question #4
-let questionFour = 'Is my favorite color is yellow? Type yes/no or y/n.';
-let answerFour = prompt(questionFour).toLowerCase();
-if (answerFour === 'yes' || answerFour === 'y') {
-  // console.log(correctStatement);
-  alert(correctStatement);
-} else if (answerFour === 'no' || answerFour === 'n') {
-  // console.log(wrongStatement);
-  alert(wrongStatement);
-} else {
-  // console.log(invalidStatement);
-  alert(invalidStatement);
-}
-
-// question #5
-let questionFive = 'Am I female? Type yes/no or y/n.';
-let answerFive = prompt(questionFive).toLowerCase();
-if (answerFive === 'no' || answerFive === 'n') {
-  // console.log(correctStatement);
-  alert(correctStatement);
-} else if (answerFive === 'yes' || answerFive === 'y') {
-  // console.log(wrongStatement);
-  alert(wrongStatement);
-} else {
-  // console.log(invalidStatement);
-  alert(invalidStatement);
-}
-
-// final message to the users
-let lastMessage = `This is the end of the game, ${userName}. Thanks for playing!`;
-// console.log(lastMessage);
-alert(lastMessage);
+// tell the users scores
+alert(`This is the end of the game, ${userName}. You got ${numberOfCorrectAnswer} out of 7 correct!`);
